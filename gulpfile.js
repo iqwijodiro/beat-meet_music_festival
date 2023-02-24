02b39c72a8ac98ea2e3f2565659e66fd10ea6bc7
Winder Díaz <winderdiaz29@gmail.com>
2023-02-23 21:13:54 -0400

    2nd commit: sections added, gulpfile: sharped.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,65 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+// Styles
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Images
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+const terser = require('gulp-terser-js');
 
 function css (done) {
     src('src/scss/**/*.scss')
-        .pipe( sass() )
+        .pipe(sourcemaps.init())
         .pipe( plumber() )
+        .pipe( sass() )
+        .pipe( postcss( [ autoprefixer(), cssnano() ]))
+        .pipe(sourcemaps.write('.'))
         .pipe( dest('build/css') )
+    done()
+}
+
+function minify (done) {
+    const options = {
+        optimizationLevel: 3
+    }
+    src('src/assets/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(options)))
+        .pipe( dest('build/img'))
+}
+
+function imagesWebp (done) {
+    const options = {
+        quality: 50
+    }
+    src('src/assets/img/**/*.{png,jpg}')
+        .pipe( webp(options) )
+        .pipe( dest('build/img'));
+    done();
+}
+function imagesAvif (done) {
+    const options = {
+        quality: 50
+    }
+    src('src/assets/img/**/*.{png,jpg}')
+        .pipe( avif(options) )
+        .pipe( dest('build/img'));
+    done();
+}
+function script (done) {
+    src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser())
+        .pipe( sourcemaps.write() )
+        .pipe(dest('build/js'))
     done()
 }
 
@@ -16,4 +69,8 @@ function dev (done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.script = script;
+exports.minify = minify;
+exports.imagesWebp = imagesWebp;
+exports.imagesAvif = imagesAvif;
+exports.dev = parallel( minify, imagesWebp, imagesAvif, script, dev );
---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+const imageContainer = document.querySelector('.image-container');
+const leftBtn = document.querySelector('#left');
+const rightBtn = document.querySelector('#right');
+const img = document.querySelectorAll('.image-container picture');
+
+let i = 0
+
+let interval = setInterval(run, 3000)
+
+const run = ()=>{
+    i++;
+    changeImg()
+}
+
+const changeImg = ()=> {
+
+    if (i > img.length - 1) {
+        i = 0
+    } else if (i < 0) {
+        i = img.length - 1;
+    }
+    
+    imageContainer.style.transform = `translateX(${-i * 500}px)`
+} 
+
+const resetInterval = () => {
+    clearInterval(interval);
+    interval = setInterval(run, 3000)
+}
+
+rightBtn.addEventListener('click', function() {
+    i++;
+    changeImg();
+    resetInterval;
+})
+leftBtn.addEventListener('click', function() {
+    i--;
+    changeImg();
+    resetInterval;
+})
